fd4d54a3572851efc8907310307efee813ee7be6
Adi3000 <adi3000@free.fr>
2021-07-09 09:21:12 +0200

    Add script to properly start zigate

--- etc/zigbee/gpio/gpio_startup.sh
+++ etc/zigbee/gpio/gpio_startup.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-gpio mode 0 out
-gpio mode 2 out
-gpio write 2 1 
-gpio write 0 0
-gpio write 0 1
-/usr/bin/socat /dev/serial0,b115200,raw,echo=0 tcp4-listen:9999
+/bin/bash /home/pi/domoticz/plugins/Domoticz-Zigate/Tools/pi-zigate.sh run
