67421b444bebca19912b382bb5c71d37f77985f9
Adi3000 <adi3000@free.fr>
2021-10-30 20:55:31 +0200

    Force switchbot to retry to click a fixed number of time

--- domoticz/switchbot_click.sh
+++ domoticz/switchbot_click.sh
@@ -3,10 +3,16 @@
 script=/home/pi/git/python-host/switchbot_py3.py
 device=D6:20:E5:47:9E:94
 loop=$1
-wait=$(test -n "$2" && echo "$2" || echo "0")
-sleep $wait
+off=$(test -n "$2" && echo "$2" || echo "0")
+wait=$(test -n "$3" && echo "$3" || echo "0")
 for i in $(seq $1); do
 	sleep 2
 	until /usr/bin/python3 $script -d $device -c press ; do sleep 5; done
 done
-
+if [ -n "$2" ]; then
+  sleep $wait
+  for i in $(seq $2); do
+        sleep 2
+        until /usr/bin/python3 $script -d $device -c press ; do sleep 5; done
+  done
+fi
