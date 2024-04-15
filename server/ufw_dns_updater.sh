128909af593052d85e3d61816caa6c8036c06cc3
adi3000 <adi300000@gmail.com>
2024-04-15 11:27:54 +0200

    Add blocky conf updater

--- server/ufw_dns_updater.sh
+++ server/ufw_dns_updater.sh
@@ -4,6 +4,8 @@ file_list=/etc/noip/domain.lst
 
 for domain in $(cat $file_list); do
 	resolvedip=$(getent hosts $domain | awk '{ print $1 }')
+	subdomain=$(echo $domain | cut -d. -f 1)
 	/usr/sbin/ufw route allow proto tcp from $resolvedip to any port 53
 	/usr/sbin/ufw route allow proto udp from $resolvedip to any port 53
+	yq -y -i '.clientLookup.clients."'$subdomain'" = ["'$resolvedip'"]' /etc/blocky/config.yml
 done
