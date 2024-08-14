de6b0f107783fc5b4b242efba3a20ad9812070ba
adi3000 <adi300000@gmail.com>
2024-08-14 12:39:31 +0200

    add Rhasspy config for server

--- server/ufw_dns_updater.sh
+++ server/ufw_dns_updater.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 file_list=/etc/noip/domain.lst
+docker_ports=(53 12183)
 
 for domain in $(cat $file_list); do
 	resolvedip=$(getent hosts $domain | awk '{ print $1 }')
 	subdomain=$(echo $domain | cut -d. -f 1)
-	/usr/sbin/ufw route allow proto tcp from $resolvedip to any port 53
-	/usr/sbin/ufw route allow proto udp from $resolvedip to any port 53
+	for port in ${docker_ports[@]}; do
+            /usr/sbin/ufw route allow proto tcp from $resolvedip to any port $port
+	    /usr/sbin/ufw route allow proto udp from $resolvedip to any port $port
+        done
 	yq -y -i '.clientLookup.clients."'$subdomain'" = ["'$resolvedip'"]' /etc/blocky/config.yml
 done
