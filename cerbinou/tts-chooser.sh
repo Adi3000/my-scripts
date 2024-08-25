288fd7461e2cdc775d35b5ec411efca90f6b97d6
Adi3000 <adi3000@free.fr>
2024-08-25 22:17:05 +0200

    feat(cerbinou): sanitize TTS text

--- cerbinou/tts-chooser.sh
+++ cerbinou/tts-chooser.sh
@@ -6,15 +6,16 @@ input_text="$@"
 if [ -z "$input_text" ]; then
 	read input_text
 fi
+sanitized_input_text=$(echo $input_text | sed -e s/\"/\'/g)
 cloud_filename="/tmp/cloud_$(date +%s%N | md5sum | head -c 8).wav"
 touch $cloud_filename
-curl -s -X POST 'https://rhasspy.adi3000.com/api/text-to-speech' --data-raw "$input_text" --connect-timeout 10 --output $cloud_filename 2> /dev/null &
+curl -s -X POST 'https://rhasspy.adi3000.com/api/text-to-speech' --data-raw "$sanitized_input_text" --connect-timeout 10 --output $cloud_filename 2> /dev/null &
 cloud_pid=$!
 
 # Start the second child process in the background and capture its PID
 melotts_filename="/tmp/melo_$(date +%s%N | md5sum | head -c 8).wav"
 touch $melotts_filename
-curl -s -X POST 'http://192.168.0.21:8381/convert/tts' -H "Content-Type: application/json" --data-raw "{\"text\":\"$input_text\",\"speed\":\"1.2\"}" --connect-timeout 10 --output $melotts_filename 2> /dev/null &
+curl -s -X POST 'http://192.168.0.21:8381/convert/tts' -H "Content-Type: application/json" --data-raw "{\"text\":\"$sanitized_input_text\",\"speed\":\"1.2\"}" --connect-timeout 10 --output $melotts_filename 2> /dev/null &
 melo_pid=$!
 
 # Loop until one of the processes completes
