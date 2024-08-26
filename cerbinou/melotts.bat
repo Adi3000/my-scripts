d6d287ae713c9ae3b2ab41f3fff3db6edc7dfd13
Adi3000 <adi300000@gmail.com>
2024-08-26 23:23:25 +0200

    feat(cerbinou): add services for Windows

--- cerbinou/melotts.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Step 1: Capture the WSL IP address using the given command
-for /f %%i in ('wsl ip -4 -o addr show eth0 ^| tr -s " " ^| cut -d" " -f4 ^| cut -d/ -f1') do set WSL_IP=%%i
-
-REM Step 2: Display the captured WSL IP address
-echo The WSL IP address is: %WSL_IP%
-
-wsl docker run --detach --name=melotts --env=DEFAULT_LANGUAGE=FR --env=DEFAULT_SPEAKER_ID=FR --env=DEFAULT_SPEED=1 -p 8381:8080 --restart=unless-stopped --volume /mnt/e/workspace/my-scripts/cerbinou/tts:/tts timhagel/melotts-api-server python app.py
