9f84a1050275a42c2aa532a0e1c37d010bfd9d22
Adi .John <adi300000@gmail.com>
2025-05-26 17:05:34 +0000

    Update script for the new server

---
+++ docker/nextcloud/README.md
@@ -0,0 +1,4 @@
+```
+chown -R www-data:www-data /etc/nextcloud
+chown -R www-data:www-data /mnt/nanopi/data/nextcloud/owncloud/data
+```
--- server/renew-cert.sh
+++ server/renew-cert.sh
@@ -6,6 +6,10 @@
 #ldap_certs=/etc/letsencrypt/live/adi3000.com
 #ldap_cert_out=/etc/ldap/ssl
 ovh_conf=/etc/letsencrypt/keys/ovh.ini
+certbot_deploy_key=/etc/ssl/private/certbot-deploy/certbot.key
+certbot_deploy_user=certbot
+certbot_deploy_target=vpsfront.adi3000.com
+
 
 /usr/bin/certbot renew  --dns-ovh --dns-ovh-credentials $ovh_conf
 # Paused for now but still working
@@ -13,4 +17,7 @@ ovh_conf=/etc/letsencrypt/keys/ovh.ini
 #cp $ldap_certs/privkey.pem $ldap_certs/cert.pem $ldap_certs/chain.pem $ldap_certs/fullchain.pem $ldap_cert_out
 #chown plex:plex $plex_cert_out
 #chown -R openldap:openldap $ldap_cert_out
+
+scp -i $certbot_deploy_key -r $ldap_certs ${certbot_deploy_user}@${certbot_deploy_target}:./certs
+ssh -r $certbot_deploy_key ${certbot_deploy_user}@${certbot_deploy_target}  "sudo mv ~/certs/* /etc/letsencrypt/live/ && sudo chown -R root:root /etc/letsencrypt/live"
 docker compose -f /home/adi/git/my-scripts/docker/traefik/docker-compose.yml --env-file /etc/default/traefik restart
