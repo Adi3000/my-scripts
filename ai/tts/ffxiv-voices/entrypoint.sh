de0b85786507505a76d5a78f4c7ad082c23c4dbc
Adrien .John <adi300000@gmail.com>
2026-07-20 15:47:09 +0200

    Update entrypoint.sh to handle symlink creation safely
    Ensure symbolic link creation does not fail if it already exists.

--- ai/tts/ffxiv-voices/entrypoint.sh
+++ ai/tts/ffxiv-voices/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /data/voices /data/voices_overrides /root/.cache /workspace/cache/huggingface
-ln -s /workspace/cache/huggingface /root/.cache/huggingface
+ln -s /workspace/cache/huggingface /root/.cache/huggingface  || true
 wget https://raw.githubusercontent.com/Adi3000/my-scripts/refs/heads/main/ai/tts/ffxiv-voices/generate_voice_fr.py -O /workspace/generate_voice_fr.py
 wget https://nextcloud.adi3000.com/public.php/dav/files/$NEXTCLOUD_SHARE_TOKEN/voices?accept=zip -O /data/voices.zip
 unzip -o  /data/voices.zip -d  /data 
