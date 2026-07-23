d8118319831533a171bf435540c801cbaf5060e6
adi <Adi3000>
2026-07-23 02:12:12 +0200

    feat(ffxivv): Download files one by one

--- server/runpod/chatterbot-tts/handler.py
+++ server/runpod/chatterbot-tts/handler.py
@@ -4,16 +4,22 @@ import os
 import sys
 import io
 import base64
+import urllib
 from chatterbox.tts import ChatterboxTTS
 from huggingface_hub import hf_hub_download
 from safetensors.torch import load_file
 import soundfile as sf
 import runpod
+import os.path
+
+
 
 MODEL_REPO = os.environ.get("MODEL_REPO","Thomcles/Chatterbox-TTS-French")
 CHECKPOINT_FILENAME = "t3_cfg.safetensors"
 VOICES_DIR = os.environ.get('VOICES_DIR', "/data/voices")
 YOUR_NAME = os.environ.get('YOUR_NAME',"Notre cher ami")
+NEXTCLOUD_URL = os.environ.get('NEXTCLOUD_URL', "https://cloud.example.com" )
+NEXTCLOUD_SHARE_TOKEN = os.environ.get('NEXTCLOUD_SHARE_TOKEN')
 CHATTERBOX_EXGERATION = float(os.environ.get('CHATTERBOX_EXGERATION',"0.8"))
 CHATTERBOX_TEMPERATURE = float(os.environ.get('CHATTERBOX_TEMPERATURE',"0.6"))
 CHATTERBOX_CFG_WEIGHT = float(os.environ.get('CHATTERBOX_CFG_WEIGHT',"0.5"))
@@ -45,6 +51,11 @@ def handler(job):
     name_value = input.get('name_value', YOUR_NAME)  
     text = prompt.replace("_NAME_", name_value)
     voice_id = input.get('voice_id')
+    if not os.path.isfile(fname): 
+        print(f"Dowloading <{voice_id}> from {NEXTCLOUD_URL}/public.php/dav/files/{NEXTCLOUD_SHARE_TOKEN}/voices/{voice_id}.wav")
+        download_file = urllib.URLopener()
+        download_file.retrieve(f"{NEXTCLOUD_URL}/public.php/dav/files/{NEXTCLOUD_SHARE_TOKEN}/voices/{voice_id}.wav", f"{VOICES_DIR}/{voice_id}.wav")
+
     waveform = synthesize_speech(model, text, audio_prompt_path=f"{VOICES_DIR}/{voice_id}.wav")
     wav = io.BytesIO()
     sf.write(wav, waveform.squeeze().cpu().numpy(), model.sr, format="WAV")
